d288fbb8e26fc81f5ec31284dc815b967926a223
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-06-07 20:28:20 +0600

    modal added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Components/Card/Card";
 import Navbar from "./Components/Navbar/Navbar";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
 
 function App() {
   const [guns, setGuns] = useState([]);
   console.log(guns);
   const [cart, setCart] = useState([]);
   console.log(cart);
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   const handelAddToCart = (gun) => {
-    const newCart = [gun];
+    const newCart = [...cart, gun];
     setCart(newCart);
   };
 
@@ -19,20 +42,24 @@ function App() {
       .then((res) => res.json())
       .then((data) => setGuns(data));
   }, []);
+
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <div>
-        {cart.map((item) => (
-          <h1 key={item.id}>{item.name}</h1>
-        ))}
-      </div>
+      <Navbar openModal={openModal}></Navbar>
 
       <div className="card-container">
         {guns.map((gun) => (
           <Card key={gun.id} gun={gun} handelAddToCart={handelAddToCart} />
         ))}
       </div>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Example Modal">
+        <button onClick={closeModal}>Close</button>
+        <div>
+          {cart.map((item) => (
+            <h1 key={item.id}>{item.name}</h1>
+          ))}
+        </div>
+      </Modal>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ openModal }) => {
   return (
     <nav className="navbar">
       <h1>বিসমিল্লাহীর রহমানির রহীম</h1>
       <h1>The Guns bucket</h1>
+      <button onClick={openModal}>Open Modal</button>
     </nav>
   );
 };
